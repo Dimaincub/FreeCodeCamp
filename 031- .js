3ead1fa0acde7a432c90c8f90cc7a383ee216659
Dimaincub <rdmitryknyazev@mail.ru>
2022-06-15 00:02:14 +0300

    come 31-36

---
+++ 031- .js
@@ -0,0 +1,30 @@
+// 31
+const someAdjective = 'awesome!'
+let myStr = 'Learning to code is '
+myStr += someAdjective
+
+// 32
+// Setup
+let lastNameLength = 0
+const lastName = 'Lovelace'
+
+// Only change code below this line
+lastNameLength = lastName.length
+
+// 32
+// Setup
+let firstLetterOfLastName = ''
+const lastName = 'Lovelace'
+
+// Only change code below this line
+firstLetterOfLastName = lastName[0] // Change this line
+
+// 33
+// Setup
+let myStr = 'Jello World'
+
+// Only change code below this line
+myStr = 'Hello World' // Change this line
+// Only change code above this line
+
+// 34
